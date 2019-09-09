cd65716d3cda9eb3f38bf301494766439a142e52
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-09-09 22:49:41 +0900

    component

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Input } from 'reactstrap';
+import { reducer, initialState } from '../reducer';
+import { actions } from '../actions';
+import useThunkReducer from 'react-hook-thunk-reducer';
+
+const StyledInput = styled(Input)`
+  margin-top: 20px;
+`;
+
+const Index: React.FC = () => {
+  const [state, dispatch] = useThunkReducer(reducer, initialState);
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    dispatch(actions.handleChange(event.currentTarget.value));
+  };
+
+  return (
+    <StyledInput
+      onChange={handleChange}
+      value={state.password}
+      type="text"
+      placeholder="password"
+    />
+  );
+};
+
+export default Index;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,42 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Row, Col, Form, Input } from 'reactstrap';
-import { reducer, initialState } from '../reducer';
-import { actions } from '../actions';
-import useThunkReducer from 'react-hook-thunk-reducer';
+import { Container, Row, Col, Form } from 'reactstrap';
+import PasswordInput from '../components/PasswordInput';
 
 const StyledContainer = styled(Container)`
   text-align: center;
 `;
 
-const StyledInput = styled(Input)`
-  margin-top: 20px;
-`;
-
-const Index: React.FC = () => {
-  const [state, dispatch] = useThunkReducer(reducer, initialState);
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    dispatch(actions.handleChange(event.currentTarget.value));
-  };
-
-  return (
-    <StyledContainer>
-      <Row>
-        <Col xs={12}>
-          <Form>
-            <StyledInput
-              onChange={handleChange}
-              value={state.password}
-              type="text"
-              placeholder="password"
-            />
-          </Form>
-        </Col>
-      </Row>
-    </StyledContainer>
-  );
-};
+const Index: React.FC = () => (
+  <StyledContainer>
+    <Row>
+      <Col xs={12}>
+        <Form>
+          <PasswordInput />
+        </Form>
+      </Col>
+    </Row>
+  </StyledContainer>
+);
 
 export default Index;
