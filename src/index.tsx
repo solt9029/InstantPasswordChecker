3466654dc1e2b0bf644eb2ad3fb550b6366f16b0
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-09-11 22:26:11 +0900

    works

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
