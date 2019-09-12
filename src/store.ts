ef36c8ef9ababe3b74b8dfc3889c9592206cbd63
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-09-12 21:15:01 +0900

    interface

--- src/store.ts
+++ src/store.ts
@@ -2,9 +2,15 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import * as reducers from './reducers';
 import history from './history';
+import { State as AppState } from './reducers/app';
+
+export interface State {
+  app: AppState;
+  router: any;
+}
 
 export default createStore(
-  combineReducers({
+  combineReducers<State>({
     ...reducers,
     router: connectRouter(history),
   }),
--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ export default {
       const prevStr = localStorage.getItem(key);
       const prevArr = prevStr ? prevStr.split(',') : [];
       const arr = [...prevArr, value];
+      console.log(arr);
       localStorage.setItem(key, arr.toString());
     },
   },
