8f3393c154f3268dd59261f5f432e57075b0f29e
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-09-14 20:41:21 +0900

    history

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Card, CardBody } from 'reactstrap';
+import styled from 'styled-components';
 
 interface Props {
   history: string[];
 }
 
+const StyledCard = styled(Card)`
+  margin-bottom: 10px;
+`;
+
 const DataList: React.SFC<Props> = ({ history }: Props) => (
   <>
     {history.map((value, index) => {
-      return <div key={index}>{value}</div>;
+      return (
+        <StyledCard key={index}>
+          <CardBody>{value}</CardBody>
+        </StyledCard>
+      );
     })}
   </>
 );
--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
 import DataList from '../containers/DataList';
+import styled from 'styled-components';
+
+const StyledContainer = styled(Container)`
+  text-align: center;
+`;
 
 const Index: React.FC = () => (
-  <Container>
-    <Row>
-      <Col xs={12}>
-        <DataList />
-      </Col>
-    </Row>
-  </Container>
+  <>
+    <Jumbotron>
+      <StyledContainer>
+        <h1>Password History</h1>
+      </StyledContainer>
+    </Jumbotron>
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <DataList />
+        </Col>
+      </Row>
+    </Container>
+  </>
 );
 
 export default Index;
