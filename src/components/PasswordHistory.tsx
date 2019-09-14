9e6b77ad9d2c2e727ef91dfb4d7dfe03ddee0bc9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-09-14 21:17:58 +0900

    item

--- src/components/PasswordHistory.tsx
+++ src/components/PasswordHistory.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { Card, CardBody } from 'reactstrap';
-import styled from 'styled-components';
+import PasswordHistoryItem from './PasswordHistoryItem';
 
 interface Props {
   history: string[];
 }
 
-const StyledCard = styled(Card)`
-  margin-bottom: 10px;
-`;
-
 const PasswordHistory: React.SFC<Props> = ({ history }: Props) => (
   <>
     {history.map((value, index) => {
-      return (
-        <StyledCard key={index}>
-          <CardBody>{value}</CardBody>
-        </StyledCard>
-      );
+      return <PasswordHistoryItem key={index}>{value}</PasswordHistoryItem>;
     })}
   </>
 );
